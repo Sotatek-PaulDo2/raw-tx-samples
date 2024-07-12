60f763df9058cafffb99dcd1abb31ea3252f22a7
Sotatek-PaulDo2 <paul.do2@sotatek.com>
2024-07-12 16:25:04 +0700

    feat: update import_address_index script

--- import_address_index.py
+++ import_address_index.py
@@ -15,16 +15,39 @@ def generate_addr_index_update_statements(
     num_addr=15,
     col_name="full_text_index",
 ): 
-    for _id_start in range(id_start, id_end, batch_size):
+    for _id_start in range(id_start, id_end + 1, batch_size):
         _id_end = _id_start + batch_size - 1
         yield generate_addr_index_update_statement(_id_start, _id_end, num_addr, col_name)
 
 
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import sessionmaker
+
+# PostgreSQL connection details
+username = "myuser"
+password = "mypassword"
+host = "127.0.0.1"
+port = "5432"
+database = "postgres"
+tx_table_name = "transaction"
+tx_type_table_name = "transaction_type"
+
+# Create SQLAlchemy engine
+engine = create_engine(
+    f"postgresql+psycopg2://{username}:{password}@{host}:{port}/{database}"
+)
+
 if __name__ == "__main__":
     buffer_size = 10
     buffer = list[str]()
-    for stmt in generate_addr_index_update_statements(1, 100_000):
+    create_session = sessionmaker(engine, autoflush=True)
+    session = create_session()
+    id_end = session.execute(text("SELECT MAX(id) FROM transaction")).one()[0]
+    print("updating from id 1 until id ", id_end)
+    for stmt in generate_addr_index_update_statements(1, id_end, col_name="free_text_index"):
         if len(buffer) == buffer_size:
+            for _stmt in buffer:
+                session.execute(text(_stmt))
             print(buffer)
             print()
             buffer.clear()
